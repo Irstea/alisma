29e0ac649f58c0d00a95f5bde7d9d3054037bab6
Eric Quinton <eric.quinton@irstea.fr>
2018-02-06 09:58:19 +0100

    transformation du fichier lisezmoi.txt en news.txt
    corrections techniques (suppression d'includes inutiles)

--- src/database/Stations.java
+++ src/database/Stations.java
@@ -5,7 +5,6 @@ package database;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
--- src/database/Taxon.java
+++ src/database/Taxon.java
@@ -2,7 +2,6 @@ package database;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Hashtable;
